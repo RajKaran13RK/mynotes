1305030d2eff2c49916eef72a77a9df9a3bcfff4
RajKaran13RK <karan.haldankar.9999@gmail.com>
2024-01-17 16:51:21 +0530

    step-13

--- test/auth_test.dart
+++ test/auth_test.dart
@@ -93,7 +93,7 @@ class MockAuthProvider implements AuthProvider {
     if (!isInitialized) throw NotInitializedException();
     if (email == 'rk13@gmail.com') throw UserNotFoundAuthException();
     if (password == 'rk13rk13') throw WrongPasswordAuthException();
-    const user = AuthUser(isEmailVerified: false);
+    const user = AuthUser(isEmailVerified: false, email: 'somegmail@gmail.com');
     _user = user;
     return Future.value(user);
   }
@@ -113,7 +113,8 @@ class MockAuthProvider implements AuthProvider {
     if (!isInitialized) throw NotInitializedException();
     final user = _user;
     if (user == null) throw UserNotFoundAuthException();
-    const newUser = AuthUser(isEmailVerified: true);
+    const newUser =
+        AuthUser(isEmailVerified: true, email: 'somegmail@gmail.com');
     _user = newUser;
   }
 }
